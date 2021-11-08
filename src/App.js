ac8cc379b86c0375376f18ab25fe3dfb90919eb4
Liam-gosnell <liamgoz99@gmail.com>
2021-11-08 21:14:59 +0000

    Added chart.js from react-chartjs, data feeding from json

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import PostList from './posts/PostList'
+import './App.css'
+import BarChart from './components/BarChart';
+import Data from './data.json';
 
 function App() {
   return (
-    <div>
-      <PostList/>
+    <div className="posts">
+        { Data.map(post => {
+          return (
+            <div key={ post.mediaId }>
+            <h4>Frame Rate - { post.framerate } </h4>
+            <p>Name - { post.name }</p>
+            <p>Start time - { post.startTime }</p>
+            <p>End time - { post.endTime }</p>
+            <p>Width - { post.width }</p>
+            <p>Height - { post.height }</p>
+            {/* <BarChart/> */}
+            </div>
+          )
+        })}
     </div>
+    
   );
 }
 
